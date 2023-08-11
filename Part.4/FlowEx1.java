b158b9baa74504b85c733dfa5b5fc06e256cd16f
samac12 <rnjsghd97@naver.com>
2023-08-11 17:50:27 +0900

    Revert "Revert "Part.3""
    This reverts commit 944746765a06537dff35d468fac4db7d02b1b0c8.

---
+++ Part.4/FlowEx1.java
@@ -0,0 +1,25 @@
+package 연습.part4;
+
+
+public class FlowEx1 {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.printf("x=%d 일떄 , 참인것은%n", x);
+
+        if(x==0) System.out.printf("x==0");
+        if(x!=0) System.out.printf("x!=0");
+        if(!(x==0)) System.out.printf("!(x==0)");
+        if(!(x!=0)) System.out.printf("!(x!=0)");
+
+        x = 1;
+        System.out.printf("x=%d일떄, 참인것은%n,", x);
+        if(x==0) System.out.printf("x!==0");
+        if(x!=0) System.out.printf("x!=0");
+        if(!(x==0)) System.out.printf("!(x==0");
+        if(!(x!=0)) System.out.printf("!(x!=0)");
+
+
+
+        }
+    }
+
