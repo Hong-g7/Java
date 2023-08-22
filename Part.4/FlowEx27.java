a551b18f190f2bdb877df02e96038483b855791c
samac12 <rnjsghd97@naver.com>
2023-08-22 11:41:11 +0900

    Part.4

---
+++ Part.4/FlowEx27.java
@@ -0,0 +1,27 @@
+package 연습.part4;
+
+import java.util.Scanner;
+
+public class FlowEx27 {
+    public static void main(String[] args) {
+        int num;
+        int sum = 0;
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("합계를 구할 숫자를 입력하세요: (끝내려면 0을 입력하세요)");
+        while (flag) {
+            System.out.println(">>");
+
+            String tmp = sc.nextLine();
+            num = Integer.parseInt(tmp);
+
+            if (num != 0) {
+                sum += num;
+            } else {
+                flag = false;
+            }
+        }
+        System.out.printf("합계:"+sum);
+    }
+}
